07463c74d43a16841eab3f21f85f5f4acf4eccb2
John Lake <jaslwolf20@gmail.com>
2024-10-09 22:39:32 -0700

    user input of fruit

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-            System.out.println( "Oh haaaaiiii!!!");
-            System.out.println( "Hi again!");
+    
+    
+
+
+
+    public static void main(String[] args) {  
+        try (Scanner scanner = new Scanner(System.in)){
+            System.out.print("What is your favorite fruit");
+            String fruit = scanner.nextLine();
+            System.out.println( "your favorite fruit is" + fruit);
+        }
+        
+            
     }
     
 }
