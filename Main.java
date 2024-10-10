19ecc0cb667b3d26b48fab8e7b8f124e38e77178
John Lake <jaslwolf20@gmail.com>
2024-10-10 11:25:23 -0700

    user input of trying to fix

--- Main.java
+++ Main.java
@@ -7,10 +7,14 @@ public class Main {
 
 
     public static void main(String[] args) {  
-        try (Scanner scanner = new Scanner(System.in)){
+        
             System.out.print("What is your favorite fruit");
-            String fruit = scanner.nextLine();
-            System.out.println( "your favorite fruit is " + fruit +"!");
+            Scanner Fruit = new Scanner(System.in);
+            Scanner NumberOfFruit = new Scanner(System.in);
+            String favFruit = Fruit.nextLine();
+            int Num1 = NumberOfFruit.nextInt();
+            for(int i = 0; i<Num1; i++){
+            System.out.println( "your favorite fruit is " + favFruit +"!");
         }
         
             
